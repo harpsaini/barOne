a054b86b0ad51e34e3bfe4483cee2a8e6c954d63
harpsaini <harpreetsinghsn@gmail.com>
2021-11-18 19:16:32 -0500

    fixed minor responsiveness issues and did some clean up

--- index.js
+++ index.js
@@ -3,19 +3,37 @@
 
 //when the click happens, the nav list will appear and hamburger be replaced with the cross, clicking the cross will hide the menu 
 
-const hamburgerMenuClick = document.querySelector('.menuBtn');
-const hamburgerMenu = document.querySelector('ul');
+const app = {};
 
-let menuOpen = false;
+ app.hamburgerAndMenu = function(){
+  let menuOpen = false;
+  app.hamburgerMenuClick.addEventListener('click',function(){
+    menuOpen = !menuOpen
+    if (menuOpen){
+      app.hamburgerMenuClick.classList.add('open');
+      app.hamburgerMenu.style.right = "0px"
+    }else{
+      app.hamburgerMenuClick.classList.remove('open');
+      app.hamburgerMenu.style.right = "-200px"
+    }
+  });
+ }
 
-hamburgerMenuClick.addEventListener('click',function(){
-  menuOpen = !menuOpen
+//  this function is resposible for the pop up box after the page load
+ app.popPageOnPageLoad = function(){
+  window.addEventListener("load",()=>{
+    app.popUpBox = document.querySelector('.popUpBox');
+    app.popUpBox = document.createElement('p');
+  });
+ }
+ app.popPageOnPageLoad ();
 
-  if (menuOpen){
-    hamburgerMenuClick.classList.add('open');
-    hamburgerMenu.style.right = "0px"
-  }else{
-    hamburgerMenuClick.classList.remove('open');
-    hamburgerMenu.style.right = "-200px"
-  }
-});
+
+  
+app.init = function(){
+  app.hamburgerMenuClick = document.querySelector('.menuBtn');
+  app.hamburgerMenu = document.querySelector('ul');
+  app.hamburgerAndMenu();
+};
+
+app.init();
